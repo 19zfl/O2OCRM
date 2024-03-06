97d77d2863f68a44e45390535cca8f073c354fa0
zfl19 <19aleiya@gmail.com>
2024-03-06 23:37:24 +0800

    startup class

---
+++ src/main/java/com/o2ocrm/O2OCRMApp.java
@@ -0,0 +1,18 @@
+package com.o2ocrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName:O2OCRMApp
+ * @Description: 程序启动类
+ * @Author:zfl19
+ * @CreateDate:2024/3/6 23:33
+ */
+
+@SpringBootApplication
+public class O2OCRMApp {
+    public static void main(String[] args) {
+        SpringApplication.run(O2OCRMApp.class,  args);
+    }
+}
