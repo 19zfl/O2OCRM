441f4d18723c56a35d60a058ac3fa5045497c9ce
zfl19 <19aleiya@gmail.com>
2024-03-12 21:01:02 +0800

    import web projects

---
+++ src/test/java/com/o2ocrm/system/basic/RedisConnectionTest.java
@@ -0,0 +1,32 @@
+package com.o2ocrm.system.basic;
+
+import com.o2ocrm.O2OCRMApp;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * @ClassName:RedisConnectionTest
+ * @Description: redis链接测试
+ * @Author:zfl19
+ * @CreateDate:2024/3/12 18:08
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = O2OCRMApp.class)
+public class RedisConnectionTest {
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Test
+    public void connTest() {
+        redisTemplate.opsForValue().set("name",  "刘十三");
+    }
+
+}
