f2caae89f8669a2845c5968f62ff0a0004d91570
zfl19 <19aleiya@gmail.com>
2024-03-26 23:37:39 +0800

    add interface pattern by sql

--- src/main/java/com/o2ocrm/system/mapper/DepartmentMapper.java
+++ src/main/java/com/o2ocrm/system/mapper/DepartmentMapper.java
@@ -3,6 +3,8 @@ package com.o2ocrm.system.mapper;
 import com.o2ocrm.system.domain.Department;
 import tk.mybatis.mapper.common.Mapper;
 
+import java.util.List;
+
 /**
  * @ClassName:DepartmentMapper
  * @Description: Department持久层
@@ -15,5 +17,11 @@ public interface DepartmentMapper extends Mapper<Department> {
     /**
      * 基本的CRUD通过继承tk提供的Mapper接口获得
      */
+
+    /**
+     * 连表查询获取部门数据
+     * @return 所有部门分页信息集合
+     */
+    List<Department> getAllDeptListBySql();
 
 }
