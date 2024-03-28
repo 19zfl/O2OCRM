9f64bbfcf32af16daa25f27e8bf69293054970a1
zfl19 <19aleiya@gmail.com>
2024-03-28 23:52:09 +0800

    add field ids

--- src/main/java/com/o2ocrm/basic/query/BaseQuery.java
+++ src/main/java/com/o2ocrm/basic/query/BaseQuery.java
@@ -1,5 +1,6 @@
 package com.o2ocrm.basic.query;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -27,5 +28,11 @@ public class BaseQuery {
      */
     @ApiModelProperty(value = "每页条数")
     private Integer pageSize;
+
+    /**
+     * id数组
+     */
+    @ApiModelProperty(value = "id数组")
+    private Long[] ids;
 
 }
